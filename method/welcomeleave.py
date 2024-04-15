ede465139fb7fcd02f73bd2c511b8099d57b025b
Pratyanj <pratyanjmodh1205@gmail.com>
2024-04-15 10:49:18 +0530

    update welcome function

--- method/welcomeleave.py
+++ method/welcomeleave.py
@@ -2,24 +2,35 @@
 import discord
 
 # print("welcomeleave.py")
-async def welcome(member, db):
+async def welcome(member,bot,db):
     server = db.collection("servers").document(str(member.guild.id))
     if not server.get().exists:
+        print(f"Server {member.guild.name} not found in database.")
+        # await member.guild.get_channel(int(member.channel.id)).send(f"Server {member.guild.name} not found in database.")
         return
     channel = server.collection("Welcome_Leave").document("welcome").get()
-    if channel.to_dict()["status"] == False or "false":
+    print("channel:",channel.to_dict())
+    if channel.to_dict()["status"] == False:
+        print(f"{member.guild.name} welcome message system is not enable.")
+        # await member.guild.get_channel(int(member.channel.id)).send(f"{member.guild.name.mention} welcome message system is not enable.")
+        return 
+    if channel.to_dict()["channel_id"] == 0:
+        print(f"Welcome channel not set for {member.guild.name}. Please set welcome channel.")
+        # await member.guild.get_channel(int(member.channel.id)).send(f"Welcome channel not set for {member.guild.name}. Please set welcome channel.") 
         return
+    else:
+        welcome_channel_id = channel.to_dict()['channel_id']
+
     if channel.to_dict()["message"] == '':
         message = f"Welcome to the server, {member.mention}! We are glad to have you."
     else:
         message = channel.to_dict()["message"]
-    if channel.to_dict()["channel_id"] == 0:
-        return
-    else:
-        welcome_channel_id = channel.to_dict()['channel_id']
+    
     
     print("welcome_channel_id:",welcome_channel_id)
-    welcome_channel = member.guild.get_channel(welcome_channel_id)
+    # gud=member.guild(member.guild.id)
+    welcome_channel = member.guild.get_channel(int(welcome_channel_id))
+    print(welcome_channel)
     welcome_message = discord.Embed(
         title="Welcome to",
         description=message,
@@ -28,17 +39,30 @@ async def welcome(member, db):
 
 
 async def Goodbye(member, db):
-    server = db.collection("servers").document(int(member.guild.id))
+    server = db.collection("servers").document(str(member.guild.id))
     if not server.get().exists :
+        print(f"Server {member.guild.name} not found in database.")
         return
     channel = server.collection("Welcome_Leave").document("leave").get()
-    if channel.to_dict()["status"] == False or "false":
+    if channel.to_dict()["status"] == False:
+        print(f"{member.guild.name} welcome message system is not enable.")
         return
-    channel = db.collection("channel_id").document("leave_channel_id").get()
-    leave_channel_id = next(iter(channel.to_dict().values()), None)
-    leave_channel = member.guild.get_channel(leave_channel_id)
+    if channel.to_dict()["channel_id"] == 0:
+        print(f"Welcome channel not set for {member.guild.name}. Please set welcome channel.")
+        return
+    else:
+        leave_channel_id = channel.to_dict()['channel_id']
+        
+    if channel.to_dict()["message"] == '':
+        print("leave message not set, using default")
+        message = f"Goodbye, {member.name}! We will miss you."
+        return
+    else:
+        message = channel.to_dict()["message"]
+
+    leave_channel = member.guild.get_channel(int(leave_channel_id))
     leave_message = discord.Embed(
         title="Goodbye",
-        description=f"Goodbye, {member.name}! We will miss you.",
+        description=message,
         color=discord.Colour.from_rgb(0, 96, 154))
     await leave_channel.send(embed=leave_message)
